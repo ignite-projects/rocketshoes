2ef1705fad02292bde2197ec0af63e928c7b7440
Robson Natanael <natanaelrobson@gmail.com>
2022-04-11 22:21:11 -0300

    feat: check localstorage

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,3 +1,4 @@
+import { totalmem } from 'os';
 import { createContext, ReactNode, useContext, useState } from 'react';
 import { toast } from 'react-toastify';
 import { api } from '../services/api';
@@ -23,11 +24,11 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
